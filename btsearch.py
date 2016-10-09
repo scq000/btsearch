9278028d752cab8fb6e184fe2c8f5ad885d6c197
一只猿 <non3gov@gmail.com>
2016-10-10 00:30:31 +0800

    Update btsearch.py
    删除了BTwalk的采集规则

--- btsearch.py
+++ btsearch.py
@@ -46,7 +46,7 @@ class tThread(threading.Thread):
         while not self.queue.empty():
             url = self.queue.get()
             try:
-                decodeHTML(url)
+                decodeBTmayi(url)
             except:
                 continue
 
@@ -76,42 +76,12 @@ def getUrlByBTmayi():
         urllist = []
 
         for pl in range(1, maxpage + 1):
-            tmpurl = {"site": "btany.com", "url": "http://www.btany.com/search/" + keyword + "-first-asc-" + str(pl)}
-            urllist.append(tmpurl)
+            urllist.append("http://www.btany.com/search/" + keyword + "-first-asc-" + str(pl))
 
         bThread(urllist)
 
     except Exception, e:
         print e
-
-
-def getUrlByBTwalk():
-    keyword = sys.argv[1]
-    url = "http://www.btmeet.net/search/" + keyword + "/1-1.html"
-    print '[*] 获取 www.btmeet.net 基本信息...'
-    try:
-        req = requests.get(url=url, verify=False, headers=headers, timeout=20)
-        responseStr = req.content.replace('\r\n', '')
-
-        totalCount = int(re.findall(r'<span>大约 (.+?) 条结果', responseStr)[0])
-        totalPage = int(math.ceil(float(totalCount) / 10))
-
-        urllist = []
-        for pl in range(1, totalPage + 1):
-            tmpurl = {"site": "btmeet.net","url": "http://www.btmeet.net/search/" + keyword + "/" + str(pl) + "-1.html"}
-            urllist.append(tmpurl)
-
-        bThread(urllist)
-
-    except Exception, e:
-        print e
-
-
-def decodeHTML(url):
-    if url['site'] == 'btany.com':
-        decodeBTmayi(url['url'])
-    elif url['site'] == 'btmeet.net':
-        decodeBTwalk(url['url'])
 
 
 def decodeBTmayi(url):
@@ -144,38 +114,6 @@ def decodeBTmayi(url):
             queryList.append(resData)
     except Exception, e:
         print e
-
-
-def decodeBTwalk(url):
-    print '[*] 正在解析 ' + url + ' 的数据...'
-    try:
-        tmpreq = requests.get(url=url, headers=headers, verify=False, timeout=20)
-        htmlstr = tmpreq.content.replace('\r\n', '')
-
-        if len(htmlstr) > 10000:
-            itemsDiv = re.findall(r'<div class="search-item">(.+?)</div>', htmlstr)
-            titles = re.findall(r'<div class="item-title">(.+?)</div>', htmlstr)
-            sizes = re.findall(r'文件大小：(.+?)</b>', htmlstr)
-
-            for x in range(0, len(itemsDiv)):
-                titleStr = re.findall(r'<h3>(.+?)</h3>', titles[x])[0]
-                titleStr = titleStr.replace('\r\n', '').replace('<b>', '').replace('</b>', '')
-                titleStr = re.findall(r'decodeURIComponent\((.+?)\)\);', titleStr)[0].replace('"+"','').replace('"','')
-                realTitle = urllib.unquote(titleStr).replace('<b>','').replace('</b>','')
-                size = sizes[x].split('>')[1]
-
-                detialUrl = 'http://www.btmeet.net' + re.findall(r'<a href="(.+?)" target="', titles[x])[0]
-                print '[*] 正在解析 ' + detialUrl + ' 的数据...'
-                detialStr = requests.get(detialUrl, timeout=10).content
-                magnet = re.findall(r'href="magnet(.+?)"', detialStr)[0]
-                thunder = re.findall(r'href="thunder(.+?)"', detialStr)[0]
-
-                resData = {"title": realTitle, "magnet": "magnet" + magnet, "thunder": "thunder" + thunder,"size": size}
-                queryList.append(resData)
-
-    except Exception, e:
-        print e
-
 
 def clearDB():
     print '[*] 清空上一次的采集数据...'
@@ -230,8 +168,5 @@ if __name__ == '__main__':
     print '[*]' + '-' * 60
 
     getUrlByBTmayi()
-    getUrlByBTwalk()
     clearDB()
     saveToDB()
-
-
